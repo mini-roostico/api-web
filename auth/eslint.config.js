17e503b17c560f3f47a42699bcd9be7e7769c8b5
Luca Rubboli <luca.rubboli2@studio.unibo.it>
2025-02-04 14:50:05 +0100

    build: add linter configs and an empty file in packages

---
+++ auth/eslint.config.js
@@ -0,0 +1,23 @@
+import eslint from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import globals from 'globals';
+import typescriptEslint from 'typescript-eslint';
+
+export default typescriptEslint.config(
+    { ignores: ['*.d.ts', '**/coverage', '**/dist'] },
+    {
+        extends: [
+            eslint.configs.recommended,
+            ...typescriptEslint.configs.recommended,
+        ],
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            globals: globals.browser,
+            parserOptions: {
+                parser: typescriptEslint.parser,
+            },
+        },
+    },
+    eslintConfigPrettier
+);
