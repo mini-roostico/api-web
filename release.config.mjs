0ca9b7971adcfa9d5f7d5ded291c8a66e3b9525b
FreshMag <pescomagnani@gmail.com>
2025-02-05 20:26:19 +0100

    build: added publish command inside release.config.mjs

--- release.config.mjs
+++ release.config.mjs
@@ -1,8 +1,15 @@
+const publishCmd = `
+cd common && npm publish --access public
+`
+
 import config from 'semantic-release-preconfigured-conventional-commits' with { type: "json" };
 
 config.plugins.push(
+    "@semantic-release/exec",
+    {
+      "publishCmd": publishCmd,
+    },
     "@semantic-release/github",
     "@semantic-release/git",
-);
-
-export default config;
+)
+export default config
