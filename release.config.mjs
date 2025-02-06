171f9edb035d26535cf4591cb8bed8b02ee6cdf7
FreshMag <pescomagnani@gmail.com>
2025-02-06 14:32:22 +0100

    ci: fixed semantic release not running due to incorrect configuration and permissions

--- release.config.mjs
+++ release.config.mjs
@@ -2,16 +2,19 @@ const publishCmd = `
 cd common
 npm version $SEMANTIC_RELEASE_NEXT_VERSION
 npm publish --access public
-`
+`;
 
 import config from 'semantic-release-preconfigured-conventional-commits' with { type: "json" };
 
 config.plugins.push(
-    "@semantic-release/exec",
-    {
-      "publishCmd": publishCmd,
-    },
+    [
+      "@semantic-release/exec",
+      {
+        "publishCmd": publishCmd
+      }
+    ],
     "@semantic-release/github",
-    "@semantic-release/git",
-)
-export default config
+    "@semantic-release/git"
+);
+
+export default config;
