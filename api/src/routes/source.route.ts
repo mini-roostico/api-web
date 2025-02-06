7538c6e44dc585202bad493e28ed60fe5f4f0140
Luca Rubboli <luca.rubboli2@studio.unibo.it>
2025-02-06 21:48:45 +0100

    feat: add source controller and route

---
+++ api/src/routes/source.route.ts
@@ -0,0 +1,33 @@
+import { Router } from "express";
+import RedisLimiterStorage from "../configs/redis.config";
+import {
+  apiLimiter,
+  ApiLimiterEntry,
+  authenticationHandler,
+} from "@mini-roostico/api-common";
+import { getSources, submitSource } from "../controllers/sources";
+
+const sourceRoute = Router();
+
+const API_LIMITER_RULES: ApiLimiterEntry = {
+  "/": {
+    GET: {
+      time: 20,
+      limit: 100,
+    },
+    POST: {
+      time: 10,
+      limit: 100,
+    },
+  },
+};
+
+const limitStorage = new RedisLimiterStorage();
+
+sourceRoute.use(apiLimiter(API_LIMITER_RULES, limitStorage));
+
+sourceRoute.get("/", authenticationHandler, getSources);
+
+sourceRoute.post("/", authenticationHandler, submitSource);
+
+export default sourceRoute;
