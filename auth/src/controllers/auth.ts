81c6c986117bfa0be8f94d718d7d91bd04761c3e
Luca Rubboli <luca.rubboli2@studio.unibo.it>
2025-02-07 17:27:22 +0100

    refactor: applied formatter

--- auth/src/controllers/auth.ts
+++ auth/src/controllers/auth.ts
@@ -1,6 +1,12 @@
 import { Request, Response, NextFunction } from "express";
-import {User, UnauthorizedError, NotFoundError, ErrorTypes, Jwt} from "@mini-roostico/api-common"
-import {StatusCodes} from "http-status-codes";
+import {
+  User,
+  UnauthorizedError,
+  NotFoundError,
+  ErrorTypes,
+  Jwt,
+} from "@mini-roostico/api-common";
+import { StatusCodes } from "http-status-codes";
 
 /**
  * Login a user and return a token pair
@@ -10,37 +16,36 @@ import {StatusCodes} from "http-status-codes";
  * @param next
  */
 export async function login(req: Request, res: Response, next: NextFunction) {
-    const email = req.body.email;
-    const password = req.body.password;
+  const email = req.body.email;
+  const password = req.body.password;
 
-    try {
-        const user = await User.findOne({email: email});
-        const responseError = new UnauthorizedError(
-            "Login error",
-            undefined,
-            ErrorTypes.LOGIN_ERROR
-        );
-        if (user === null || user === undefined) {
-            return next(responseError);
-        }
-
-        user.comparePassword(password, async function(error, isMatch) {
-            if (error || !isMatch) {
-                return next(responseError);
-            }
-
-            const tokens = await Jwt.createTokenPair(user);
-            res.locals.code = StatusCodes.OK;
-            res.locals.data = {
-                accessToken: tokens.accessToken,
-                refreshToken: tokens.refreshToken
-            };
-            return next();
-        });
-
-    } catch(error) {
-        return next(error);
+  try {
+    const user = await User.findOne({ email: email });
+    const responseError = new UnauthorizedError(
+      "Login error",
+      undefined,
+      ErrorTypes.LOGIN_ERROR,
+    );
+    if (user === null || user === undefined) {
+      return next(responseError);
     }
+
+    user.comparePassword(password, async function (error, isMatch) {
+      if (error || !isMatch) {
+        return next(responseError);
+      }
+
+      const tokens = await Jwt.createTokenPair(user);
+      res.locals.code = StatusCodes.OK;
+      res.locals.data = {
+        accessToken: tokens.accessToken,
+        refreshToken: tokens.refreshToken,
+      };
+      return next();
+    });
+  } catch (error) {
+    return next(error);
+  }
 }
 
 /**
@@ -51,24 +56,24 @@ export async function login(req: Request, res: Response, next: NextFunction) {
  * @param next
  */
 export async function logout(req: Request, res: Response, next: NextFunction) {
-    const authorizationHeader = req.headers["authorization"] as string;
-    const accessToken = authorizationHeader.split(" ")[1];
-    try {
-        await Jwt.findOneAndUpdate(
-            {accessToken: accessToken},
-            { $set: { enabled: false } },
-            { new: true, useFindAndModify: false },
-        );
+  const authorizationHeader = req.headers["authorization"] as string;
+  const accessToken = authorizationHeader.split(" ")[1];
+  try {
+    await Jwt.findOneAndUpdate(
+      { accessToken: accessToken },
+      { $set: { enabled: false } },
+      { new: true, useFindAndModify: false },
+    );
 
-        res.locals.code = StatusCodes.OK;
-        res.locals.data = {
-            message: "User logged out"
-        }
-    } catch (error) {
-        return next(error);
-    }
+    res.locals.code = StatusCodes.OK;
+    res.locals.data = {
+      message: "User logged out",
+    };
+  } catch (error) {
+    return next(error);
+  }
 
-    return next();
+  return next();
 }
 
 /**
@@ -79,55 +84,57 @@ export async function logout(req: Request, res: Response, next: NextFunction) {
  * @param res
  * @param next
  */
-export async function refreshToken(req: Request, res: Response, next: NextFunction) {
+export async function refreshToken(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const email = req.body.email;
+  const refreshToken = req.body.refreshToken;
 
-    const email = req.body.email;
-    const refreshToken = req.body.refreshToken;
+  const user = await User.findOne({ email: email });
+  if (user === null || user === undefined) {
+    return next(
+      new NotFoundError(
+        "The specified email doesn't belong to any users",
+        undefined,
+        ErrorTypes.VALIDATION_ERROR,
+      ),
+    );
+  }
 
-    const user = await User.findOne({email: email});
-    if (user === null || user === undefined) {
+  const tokenRecord = await Jwt.findOne({ refreshToken: refreshToken });
+  if (tokenRecord) {
+    try {
+      const tokenResponse = await tokenRecord.validateRefreshToken();
+      if (tokenResponse.sub.email != email) {
         return next(
-            new NotFoundError(
-                "The specified email doesn't belong to any users",
-                undefined,
-                ErrorTypes.VALIDATION_ERROR
-            )
+          new UnauthorizedError(
+            "The submitted token doesn't belong to the specified user",
+            undefined,
+            ErrorTypes.JWT_ERROR,
+          ),
         );
+      }
+      const newTokens = await Jwt.createTokenPair(user);
+
+      res.locals.code = StatusCodes.OK;
+      res.locals.data = {
+        accessToken: newTokens.accessToken,
+        refreshToken: newTokens.refreshToken,
+      };
+    } catch (error) {
+      return next(error);
     }
+  } else {
+    return next(
+      new NotFoundError(
+        "Can't find the requested token",
+        undefined,
+        ErrorTypes.JWT_ERROR,
+      ),
+    );
+  }
 
-    const tokenRecord = await Jwt.findOne({refreshToken: refreshToken});
-    if (tokenRecord) {
-        try {
-            const tokenResponse = await tokenRecord.validateRefreshToken();
-            if (tokenResponse.sub.email != email) {
-                return next(
-                    new UnauthorizedError(
-                        "The submitted token doesn't belong to the specified user",
-                        undefined,
-                        ErrorTypes.JWT_ERROR
-                    )
-                );
-            }
-            const newTokens = await Jwt.createTokenPair(user);
-
-            res.locals.code = StatusCodes.OK;
-            res.locals.data = {
-                accessToken: newTokens.accessToken,
-                refreshToken: newTokens.refreshToken
-            };
-
-        } catch (error) {
-            return next(error);
-        }
-    } else {
-        return next(
-            new NotFoundError(
-                "Can't find the requested token",
-                undefined,
-                ErrorTypes.JWT_ERROR
-            )
-        );
-    }
-
-    return next();
+  return next();
 }
