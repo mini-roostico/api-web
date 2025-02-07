ff0e23f80e49a333269ebb3b1e2c89f4916f322d
Luca Rubboli <luca.rubboli2@studio.unibo.it>
2025-02-07 17:24:52 +0100

    feat: add auth controller and route

---
+++ auth/src/routes/auth.route.ts
@@ -0,0 +1,58 @@
+import { Router } from "express";
+import {login, logout, refreshToken} from "../controllers/auth.js";
+import {apiLimiter, authenticationHandler, validationHandler, ApiLimiterEntry} from "@mini-roostico/api-common";
+import RedisLimiterStorage from "../configs/redis.config.js";
+import {body} from "express-validator";
+
+const API_LIMITER_RULES: ApiLimiterEntry = {
+    "/login": {
+        "POST": {
+            time: 20,
+            limit: 100
+        }
+    },
+    "/logout": {
+        "POST": {
+            time: 20,
+            limit: 100
+        }
+    },
+    "/refresh": {
+        "POST": {
+            time: 20,
+            limit: 100
+        }
+    }
+}
+
+const limitStorage = new RedisLimiterStorage();
+
+const authRouter = Router();
+
+authRouter.use(apiLimiter(API_LIMITER_RULES, limitStorage));
+
+authRouter.post(
+    "/login",
+    validationHandler([
+        body("email").exists().isEmail(),
+        body("password").exists().isString()
+    ]),
+    login
+);
+
+authRouter.post(
+    "/logout",
+    authenticationHandler,
+    logout
+);
+
+authRouter.post(
+    "/refresh",
+    validationHandler([
+        body("email").exists().isEmail(),
+        body("refreshToken").exists().isJWT()
+    ]),
+    refreshToken
+);
+
+export default authRouter;
