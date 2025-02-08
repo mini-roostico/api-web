45486f195c31f15d43c58fbc9fe943ae569418c3
FreshMag <pescomagnani@gmail.com>
2025-02-08 18:22:53 +0100

    style: fixed style in common package

--- common/src/middleware/authentication.middleware.ts
+++ common/src/middleware/authentication.middleware.ts
@@ -1,26 +1,26 @@
 import { NextFunction, Request, Response } from "express";
 import { BadRequestError, NotFoundError } from "../errors/errors.js";
 import { ErrorTypes } from "../errors/error.types.js";
-import {UserDocumentType} from "../models/users/users.js";
-import {IJsonWebTokenModel} from "../models/jwt/jwt.js";
+import { UserDocumentType } from "../models/users/users.js";
+import { IJsonWebTokenModel } from "../models/jwt/jwt.js";
 
 export function makeAuthenticationHandlerWithModel(
-    userModel: UserDocumentType,
-    jwtModel: IJsonWebTokenModel,
+  userModel: UserDocumentType,
+  jwtModel: IJsonWebTokenModel,
 ) {
   return async function authenticationHandler(
-      req: Request,
-      res: Response,
-      next: NextFunction,
+    req: Request,
+    res: Response,
+    next: NextFunction,
   ) {
     const authorization = req.headers.authorization;
     if (authorization == undefined) {
       return next(
-          new BadRequestError(
-              "Authorization header not found in the request",
-              undefined,
-              ErrorTypes.VALIDATION_ERROR,
-          ),
+        new BadRequestError(
+          "Authorization header not found in the request",
+          undefined,
+          ErrorTypes.VALIDATION_ERROR,
+        ),
       );
     }
     const accessToken = authorization.split(" ")[1];
@@ -28,22 +28,24 @@ export function makeAuthenticationHandlerWithModel(
       const jwtRecord = await jwtModel.findOne({ accessToken: accessToken });
       if (jwtRecord == null) {
         return next(
-            new NotFoundError(
-                "Submitted jwt token doesn't exist",
-                undefined,
-                ErrorTypes.JWT_ERROR,
-            ),
+          new NotFoundError(
+            "Submitted jwt token doesn't exist",
+            undefined,
+            ErrorTypes.JWT_ERROR,
+          ),
         );
       }
       const validationResponse = await jwtRecord.validateAccessToken();
-      const user = await userModel.findOne({ email: validationResponse.sub.email });
+      const user = await userModel.findOne({
+        email: validationResponse.sub.email,
+      });
       if (user == null) {
         return next(
-            new NotFoundError(
-                "User not found",
-                undefined,
-                ErrorTypes.VALIDATION_ERROR,
-            ),
+          new NotFoundError(
+            "User not found",
+            undefined,
+            ErrorTypes.VALIDATION_ERROR,
+          ),
         );
       }
       res.locals.user = user;
@@ -51,5 +53,5 @@ export function makeAuthenticationHandlerWithModel(
       return next(error);
     }
     next();
-  }
+  };
 }
