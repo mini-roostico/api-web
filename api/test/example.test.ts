73308714f75438d64e2fae7136d2717a8c0751d0
FreshMag <pescomagnani@gmail.com>
2025-02-08 23:24:02 +0100

    test: setup tests for api and auth

--- api/test/example.test.ts
+++ api/test/example.test.ts
@@ -1,9 +1,23 @@
-import { describe, expect, test } from "@jest/globals";
-import { JwtHandler } from "@mini-roostico/api-common";
+import {describe, expect, it} from "@jest/globals";
+// @ts-ignore
+import request from "supertest"
+import {app} from "./jest.setup";
 
-describe("When creating an example test", () => {
-    test("should compare to identical strings to be equal", () => {
-        expect("a").toBe("a");
-        JwtHandler
+
+describe("User controller", () => {
+    it("should correctly create a user", async () => {
+        const response = await request(app)
+            .post("/users")
+            .send({
+                email: "a@a.com",
+                password: "StrongPassword!234",
+                firstName: "John",
+                secondName: "Doe",
+            })
+            .expect(201)
+        const { data } = response.body
+        expect(data.email).toBe("a@a.com")
+        expect(data.firstName).toBe("John")
+        expect(data.secondName).toBe("Doe")
     });
 });
---
+++ auth/test/jest.setup.ts
@@ -0,0 +1,34 @@
+import { beforeAll, afterAll, afterEach } from "@jest/globals";
+import {MongoMemoryServer} from "mongodb-memory-server";
+import { Application } from "express";
+import ExpressConfig from "../src/configs/express.config";
+import mongoose from "mongoose";
+import {Server} from "node:net";
+
+let mongoServer: MongoMemoryServer;
+export let app: Application
+let server: Server
+
+beforeAll(async () => {
+    mongoServer = await MongoMemoryServer.create();
+    const uri = mongoServer.getUri();
+    await mongoose.connect(uri);
+    app = ExpressConfig(false);
+    server = app.listen(8081);
+});
+
+afterAll(async () => {
+    await mongoose.disconnect();
+    await mongoServer.stop();
+    if (server) {
+        await new Promise((resolve) => server.close(resolve));
+    }
+});
+
+afterEach(async () => {
+    const collections = mongoose.connection.collections;
+    for (const key in collections) {
+        const collection = collections[key];
+        await collection.deleteMany({});
+    }
+});
