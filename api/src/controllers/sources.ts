2710a12bc4bfdff32e2cc58c2ee1a2c0c87b9573
Luca Rubboli <luca.rubboli2@studio.unibo.it>
2025-02-08 19:15:17 +0100

    feat: add put, delete and save source routes

--- api/src/controllers/sources.ts
+++ api/src/controllers/sources.ts
@@ -1,8 +1,17 @@
 import { NextFunction, Request, Response } from "express";
 import { ac } from "../configs/accesscontrol.config.js";
-import { ErrorTypes, UnauthorizedError } from "@mini-roostico/api-common";
+import {
+  ErrorTypes,
+  UnauthorizedError,
+  BadRequestError,
+} from "@mini-roostico/api-common";
 import { SourceModel as Source } from "../models/models.js";
 import { StatusCodes } from "http-status-codes";
+
+async function getSource(userId: string, sourceId: string) {
+  const sources = await Source.getSourcesForUser(userId);
+  return sources.find((source) => source._id === sourceId);
+}
 
 export async function getSources(
   _req: Request,
@@ -30,48 +39,47 @@ export async function getSources(
 }
 
 export async function saveSource(
-    req: Request,
-    res: Response,
-    next: NextFunction,
+  req: Request,
+  res: Response,
+  next: NextFunction,
 ) {
+  if (!ac.can(res.locals.user.role).createAny("sources").granted) {
+    next(
+      new UnauthorizedError(
+        "Can't create the resource",
+        undefined,
+        ErrorTypes.AUTHENTICATION_ERROR,
+      ),
+    );
+  }
 
-    if (!ac.can(res.locals.user.role).createAny("sources").granted) {
-        next(
-            new UnauthorizedError(
-                "Can't create the resource",
-                undefined,
-                ErrorTypes.AUTHENTICATION_ERROR,
-            ),
-        );
-    }
+  const name: string = req.body.name;
+  const subjects: Map<string, string> = req.body.subjects;
+  const parameters: Map<string, string | number | boolean> =
+    req.body.parameters ?? new Map();
+  type Func = (...args: unknown[]) => unknown;
+  const macros: Map<string, Func> = req.body.macros ?? new Map();
+  const configurations: Map<string, unknown[]> =
+    req.body.configurations ?? new Map();
+  const last_update: Date = new Date(Date.now());
 
-    const name: string = req.body.name;
-    const subjects: Map<string, string> = req.body.subjects;
-    const parameters: Map<string, string | number | boolean> =
-        req.body.parameters ?? new Map();
-    type Func = (...args: unknown[]) => unknown;
-    const macros: Map<string, Func> = req.body.macros ?? new Map();
-    const configurations: Map<string, unknown[]> =
-        req.body.configurations ?? new Map();
-    const last_update: Date = new Date(Date.now());
+  const source = new Source({
+    name,
+    subjects,
+    parameters,
+    macros,
+    configurations,
+    user: res.locals.user._id,
+    last_update,
+  });
 
-    const source = new Source({
-        name,
-        subjects,
-        parameters,
-        macros,
-        configurations,
-        user: res.locals.user._id,
-        last_update,
-    });
-
-    try {
-        await source.save();
-        res.locals.code = StatusCodes.CREATED;
-        res.locals.data = source;
-    } catch (err) {
-        next(err);
-    }
+  try {
+    await source.save();
+    res.locals.code = StatusCodes.CREATED;
+    res.locals.data = source;
+  } catch (err) {
+    next(err);
+  }
 }
 
 export async function submitSource(
@@ -110,4 +118,90 @@ export async function submitSource(
   } catch (err) {
     next(err);
   }
+}
+
+/**
+ * Edit the source
+ * @param req
+ * @param res
+ * @param next
+ */
+export async function editSource(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const isAllowed = ac.can(res.locals.user.role).updateAny("sources").granted;
+  if (!isAllowed) {
+    next(
+      new UnauthorizedError(
+        "Can't access to the resource",
+        undefined,
+        ErrorTypes.AUTHENTICATION_ERROR,
+      ),
+    );
+  }
+
+  const data = req.body.data;
+  if (data === null || data === undefined) {
+    return next(
+      new BadRequestError(
+        'Request need to have a "data" field',
+        undefined,
+        ErrorTypes.VALIDATION_ERROR,
+      ),
+    );
+  }
+  try {
+    const source = await getSource(res.locals.user._id, data._id);
+    await Source.updateOne({ _id: source._id }, data);
+    res.locals.code = StatusCodes.OK;
+    res.locals.data = true;
+  } catch (error) {
+    return next(error);
+  }
+  return next();
+}
+
+/**
+ * Delete a source
+ * @param req
+ * @param res
+ * @param next
+ */
+export async function deleteSource(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const isAllowed = ac.can(res.locals.user.role).deleteAny("sources").granted;
+  if (!isAllowed) {
+    next(
+      new UnauthorizedError(
+        "Can't access to the resource",
+        undefined,
+        ErrorTypes.AUTHENTICATION_ERROR,
+      ),
+    );
+  }
+
+  const data = req.body.data;
+  if (data === null || data === undefined) {
+    return next(
+      new BadRequestError(
+        'Request need to have a "data" field',
+        undefined,
+        ErrorTypes.VALIDATION_ERROR,
+      ),
+    );
+  }
+  try {
+    const source = await getSource(res.locals.user._id, data._id);
+    await Source.deleteOne({ _id: source._id });
+    res.locals.code = StatusCodes.OK;
+    res.locals.data = true;
+  } catch (error) {
+    return next(error);
+  }
+  return next();
 }
--- api/src/routes/source.route.ts
+++ api/src/routes/source.route.ts
@@ -5,6 +5,13 @@ import {
   ApiLimiterEntry,
   makeAuthenticationHandlerWithModel,
 } from "@mini-roostico/api-common";
+import {
+  deleteSource,
+  editSource,
+  getSources,
+  saveSource,
+  submitSource,
+} from "../controllers/sources.js";
 import { getSources, submitSource } from "../controllers/sources.js";
 import { UserModel } from "../models/models.js";
 
@@ -27,14 +34,14 @@ const API_LIMITER_RULES: ApiLimiterEntry = {
     DELETE: {
       time: 10,
       limit: 100,
-    }
+    },
   },
   "/submit": {
     POST: {
       time: 10,
       limit: 100,
     },
-  }
+  },
 };
 
 const authenticationHandler = makeAuthenticationHandlerWithModel(UserModel);
@@ -43,10 +50,10 @@ const limitStorage = new RedisLimiterStorage();
 sourceRoute.use(apiLimiter(API_LIMITER_RULES, limitStorage));
 
 sourceRoute.get("/", authenticationHandler, getSources);
+sourceRoute.post("/", authenticationHandler, saveSource);
+sourceRoute.put("/", authenticationHandler, editSource);
+sourceRoute.delete("/", authenticationHandler, deleteSource);
 
-//sourceRoute.post("/", authenticationHandler, saveSource);
-//sourceRoute.put("/", authenticationHandler, putSource);
-//sourceRoute.delete("/", authenticationHandler, deleteSource);
 sourceRoute.post("/submit", authenticationHandler, submitSource);
 
 export default sourceRoute;
